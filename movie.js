ac76c60de64e3dfb29cf39af1c73b7453b87e3eb
R ger <118863843+KeenGWatanabe@users.noreply.github.com>
2023-01-02 23:18:12 +0000

    upto reviews header, pending input fields no working

--- movie.js
+++ movie.js
@@ -1,58 +1,63 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4f2d559c0946b05bd886ae61ab616303&page=1';
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=4f2d559c0946b05bd886ae61ab616303&query=";
+const url = new URL(location.href);
+const movieId = url.searchParams.get("id")
+const movieTitle = url.searchParams.get("title")
+
+
+const APILINK = 'https://review-backend.rger1.repl.co/api/v1/reviews';
 
 
 const main = document.getElementById("section");
-const form = document.getElementById("form");
-const search = document.getElementById("query");
+const title = document.getElementById("title");
 
-returnMovies(APILINK)
-function returnMovies(url){
-  fetch(url).then(res => res.json())
+title.innerText = movieTitle;
+
+returnReviews(APILINK);
+
+function returnReviews(url){
+  fetch(url + "movie/" + movieId).then(res => res.json())
   .then(function(data){
-  console.log(data.results);
-  data.results.forEach(element => {
+  console.log(data);
+  data.forEach(element => {
       const div_card = document.createElement('div');
-      div_card.setAttribute('class', 'card');
+      <div class="row">
+        <div class="column">
+          <div class="card" id="${review.id}">
+            <p><strong>Review: </strong>${review.review}</p>
+            <p><strong>User: </strong>${review.user}</p>
+            <p><a href="#" onclick="editReview('${review._id}','${review.review}','${review.user}')">pencil</a><a href="#" onclick="deleteReview('${review._id}')">trash</a></p>    
+      </div>
+        </div>
+        </div>
+    
+    
+    //div_card.setAttribute('class', 'card');
       
-      const div_row = document.createElement('div');
-      div_row.setAttribute('class', 'row');
+      //const div_row = document.createElement('div');
+      //div_row.setAttribute('class', 'row');
       
-      const div_column = document.createElement('div');
-      div_column.setAttribute('class', 'column');
+      //const div_column = document.createElement('div');
+      //div_column.setAttribute('class', 'column');
       
-      const image = document.createElement('img');
-      image.setAttribute('class', 'thumbnail');
-      image.setAttribute('id', 'image');
+      //const image = document.createElement('img');
+      //image.setAttribute('class', 'thumbnail');
+      //image.setAttribute('id', 'image');
       
-      const title = document.createElement('h3');
-      title.setAttribute('id', 'title');
+      //const title = document.createElement('h3');
+      //title.setAttribute('id', 'title');
       
-      const center = document.createElement('center');
+      //const center = document.createElement('center');
 
-      title.innerHTML = `${element.title}`;
-      image.src = IMG_PATH + element.poster_path;
+      //title.innerHTML = `${element.title}`;
+      //image.src = IMG_PATH + element.poster_path;
 
-      center.appendChild(image);
-      div_card.appendChild(center);
-      div_card.appendChild(title);
-      div_column.appendChild(div_card);
-      div_row.appendChild(div_column);
+      //center.appendChild(image);
+      //div_card.appendChild(center);
+      //div_card.appendChild(title);
+      //div_column.appendChild(div_card);
+      //div_row.appendChild(div_column);
 
-      main.appendChild(div_row);
+      main.appendChild(div_card);
   });
 });
 }
 
-form.addEventListener("submit", (e) => {
-e.preventDefault();
-main.innerHTML = '';
-
-const searchItem = search.value;
-
-if (searchItem) {
-  returnMovies(SEARCHAPI + searchItem);
-    search.value = "";
-}
-});
